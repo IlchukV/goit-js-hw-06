46a1229abadeb60834eafb8e095bcc8cd5002b4e
Vadim Ilchuk <vadmany@gmail.com>
2023-03-13 18:50:01 +0100

    Домашка 4

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+const counterRef = document.querySelector(`#counter`);
+// console.log(counterRef);
+
+const decrementBtnRef = counterRef.firstElementChild;
+// console.log(decrementBtnRef);
+
+const elCounterRef = counterRef.querySelector(`#value`);
+// console.log(elCounterRef);
+
+const incrementBtnRef = counterRef.lastElementChild
+// console.log(incrementBtnRef);
+
+let counter = `0`;
+
+decrementBtnRef.addEventListener(`click`, handleDecrementButtonClick);
+
+function handleDecrementButtonClick() {
+    console.log(`-1`);
+    elCounterRef.textContent = [counter -= 1];
+}
+
+incrementBtnRef.addEventListener(`click`, handleIncrementButtonClick);
+
+function handleIncrementButtonClick() {
+    console.log(`+1`);
+    elCounterRef.textContent = [counter += 1];
+}
+
