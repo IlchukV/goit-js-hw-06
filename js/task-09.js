28a556179e01efff63152a476b180d5f50d8d597
Vadim Ilchuk <vadmany@gmail.com>
2023-03-15 22:48:48 +0100

    Домашка 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,23 @@
+
+const refs = {
+  button: document.querySelector(`.change-color`),
+  text: document.querySelector(`.color`)
+    }
+refs.button.addEventListener(`click`, onClickButtonChangeColor);
+
+// функція, яка генерує рандомний колір
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+function onClickButtonChangeColor() {
+  //задаємо body інлайн стиль
+  document.body.style.backgroundColor = getRandomHexColor();
+  // виводимо в span номер кольору
+  refs.text.textContent = getRandomHexColor();
 }
+
+  
+  
