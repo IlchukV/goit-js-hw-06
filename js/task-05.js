9374b202dac4c13e714bc2910ed9ab4ab4695748
Vadim Ilchuk <vadmany@gmail.com>
2023-03-16 11:38:53 +0100

    Виправлення ДЗ-5
    використано метод trim()

--- js/task-05.js
+++ js/task-05.js
@@ -8,21 +8,7 @@ refs.input.addEventListener(`input`, onInputChange);
 
 function onInputChange(event) {
    
-    (event.currentTarget.value != ``) ? refs.namelabel.textContent = event.currentTarget.value : refs.namelabel.textContent = `Anonymous`;
+    (event.currentTarget.value.trim() !== ``) ? refs.namelabel.textContent = event.currentTarget.value : refs.namelabel.textContent = `Anonymous`;
     console.log(refs.namelabel.textContent);
    
 }
-
-
-//слухач на фокус
-// refs.input.addEventListener(`focus`, onInputFocus);
-
-// //слухач на НЕ фокус
-// refs.input.addEventListener(`blur`, onInputBlur);
-
-// function onInputFocus() {
-//     console.log(`фокус`);
-// };
-// function onInputBlur() {
-//     console.log(`блюр`);
-// };
